c1d0cc71f5aefa9f81792e6926f132cdadb53ec0
rick buring <rick@buring.nl>
2022-05-13 20:53:21 +0200

    added get method

--- Party-Vision/GameObject.hpp
+++ Party-Vision/GameObject.hpp
@@ -17,5 +17,26 @@ namespace Scene {
 		std::list<std::shared_ptr<AbstractComponent>> getComponents();
 		void update(float elapsedTime);
 
+		template<class T>
+		std::shared_ptr<T> getComponent()
+		{
+			for (auto c : components)
+			{
+				std::shared_ptr<T> t = dynamic_pointer_cast<T>(c);
+				if (t)
+					return t;
+			}
+			return nullptr;
+		}
+
+		template<class T>
+		void removeComponent()
+		{
+			components.remove_if([](Component* c)
+				{
+					T* t = dynamic_cast<T*>(c);
+					return t != nullptr;
+				});
+		}
 	};
 }
