b77e350d676dd4c85779580b1abf0c33d91a487b
Thomas <t.flantua@student.avans.nl>
2022-05-24 15:30:54 +0200

    Skin color can be detected.

--- Party-Vision/HandDetection.cpp
+++ Party-Vision/HandDetection.cpp
@@ -10,7 +10,7 @@ using namespace cv::ml;
 #define COLOR_COLS 250
 
 namespace Vision {
-	void HandDetection_init(Mat frame, vector<Rect> faces)
+	Vec3b HandDetection_init(Mat frame, vector<Rect> faces)
 	{
 		//Stuk rectangle uit het frame halen
 		Mat imgCrop = frame(faces[0]);
@@ -24,11 +24,23 @@ namespace Vision {
 			Point(width * 0.75, height * 0.25), 
 			Point(width * 0.25, height * 0.25)};
 
-		//Vec3b color = imgCrop.at<Vec3b>(Point(imgCrop.size().width / 2, imgCrop.size().height / 2));
+		int b = 0;
+		int g = 0;
+		int r = 0;
 
 		for (auto l : locations) {
 			rectangle(imgCrop, Rect(l.x - 13, l.y - 13, 26, 26), Scalar(0, 255, 0));
+			Vec3b color = imgCrop.at<Vec3b>(l);
+			cout << "Color: " << color << "\n";
+			b += color[0];
+			g += color[1];
+			r += color[2];
 		}
+		int points = locations.size();
+
+		Vec3b skinColor = Vec3b(b / points, g / points, r / points);
+
+		cout << "Skin color: " << skinColor << "\n";
 
 		imshow("Source image", imgCrop);
 		imshow("Image", frame);
--- Party-Vision/HandDetection.hpp
+++ Party-Vision/HandDetection.hpp
@@ -8,5 +8,5 @@ using namespace cv;
 using namespace cv::ml;
 
 namespace Vision {
-	void HandDetection_init(Mat frame, vector<Rect>);
+	Vec3b HandDetection_init(Mat frame, vector<Rect>);
 }
