0f797c28d6639411727c890874b53bd273fc069e
Stijn van Tilburg <stijnvtilburg@hotmail.com>
2022-06-01 16:16:29 +0200

    Auto stash before merge of "Feature-GameLogic" and "origin/Feature-GameLogic"

--- Party-Vision/SchoolNinja.cpp
+++ Party-Vision/SchoolNinja.cpp
@@ -46,11 +46,14 @@ namespace Scene {
 		 //_textureList.push_back(std::make_shared<DrawObjectComponent>(loadObject("models/paper/Papers.obj")));
 		 
 		 std::shared_ptr<GameObject> book = std::make_shared<GameObject>();
-		 book->addComponent(std::make_shared<TransformComponent>(glm::vec3(0, 0, 0), glm::vec3(-50, 100, 0)));
-		 book->addComponent(std::make_shared<GravityComponent>(80));
-		 book->addComponent(_textureList[0]);
+		 book->addComponent(std::make_shared<TransformComponent>(glm::vec3(1, 0, 0), glm::vec3(-50, 100, 0)));
+
+		 book->addComponent(_textureList[1]);
+
+
 
 		 SchoolNinja::_scene->addGameObject(book);
+		 
 
 		_objects.push_back(obj);
 	}
