4f2ccab4088d3e729bd1d0d4d642c8fe679fbe47
Imre <imre@fien-dermout.nl>
2022-06-14 15:22:44 +0200

    Added comments to some hpp files

--- Party-Vision/HudComponent.hpp
+++ Party-Vision/HudComponent.hpp
@@ -12,15 +12,49 @@ namespace Scene {
 		std::vector<GameObject> _highScore;
 		std::vector<Texture*> _numbers;
 		Scene* _scene;
+		/// <summary>
+		/// Creates the objects for the lives and adds them to the vector lives.
+		/// </summary>
 		void initLives();
+
+		/// <summary>
+		/// Creates the objects for the score and adds them to the vector score.
+		/// </summary>
 		void initScore();
+
+		/// <summary>
+		/// Creates the objects for the highscore and adds them to the vector highscore.
+		/// </summary>
 		void initHighScore();
 	public:
 		HudComponent();
+
+		/// <summary>
+		/// Removes 1 object that represent a life form the vector lives.
+		/// </summary>
 		void removeLife();
+
+		/// <summary>
+		/// Creates drawable objects that reperesent the time left in the game and adds them to the vector time.
+		/// </summary>
+		/// <param name="timeInSeconds">The time left in the game</param>
 		void setTime(int timeInSeconds);
+
+		/// <summary>
+		/// Updates the drawable object for the score in the vector score to represent the current score.
+		/// </summary>
+		/// <param name="score">The current score</param>
 		void setScore(int score);
+
+		/// <summary>
+		/// Updates the drawable object for the highscore in the vector highscore to represent the current highscore.
+		/// </summary>
+		/// <param name="score">The current highscore</param>
 		void setHighScore(int score);
+
+		/// <summary>
+		/// Draws the object present in the vectors time, lives, score and highscore.
+		/// </summary>
 		virtual void draw() override;
 	};
 };
--- Party-Vision/IncreaseScoreComponent.hpp
+++ Party-Vision/IncreaseScoreComponent.hpp
@@ -8,8 +8,15 @@
 namespace Scene {
 	class IncreaseScoreComponent : public IOnDeath, public AbstractComponent {
 	public:
+		/// <summary>
+		/// Intitializes SchoolNinjaComponent.
+		/// </summary>
+		/// <param name="">The SchoolNinjaComponent that handels the current games logic</param>
 		IncreaseScoreComponent(SchoolNinjaComponent*);
 
+		/// <summary>
+		/// Adds 100 points to the current score.
+		/// </summary>
 		void OnDeath() override;
 
 	private:
