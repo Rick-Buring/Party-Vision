661cb3a91cd4a501230ecebdedc2b3979322299d
Stijn van Tilburg <stijnvtilburg@hotmail.com>
2022-06-14 13:18:33 +0200

    More documentation

--- Party-Vision/replaceComponent.hpp
+++ Party-Vision/replaceComponent.hpp
@@ -10,8 +10,16 @@ namespace Scene {
 	class GameObject;
 	class ReplaceComponent : public IOnDeath, public AbstractComponent {
 	public:
+		/// <summary>
+		/// Will spawn another Object upon death of original Object
+		/// </summary>
+		/// <param name="obj">The object that will spawn on death</param>
+		/// <param name="scene">The scene to spawn the object into</param>
 		ReplaceComponent(std::shared_ptr<GameObject> obj, Scene* scene);
 
+		/// <summary>
+		/// Implements the OnDeath to method to spawn in the object when it is called. 
+		/// </summary>
 		void OnDeath() override;
 
 	private:
